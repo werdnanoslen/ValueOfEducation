1c7ff3f4154cdef755c6603de9e8322498a25a14
Andrew Nelson <werdnanoslen@gmail.com>
2012-12-05 20:29:46 -0500

    added front-page info

--- standalone/instructions.php
+++ standalone/instructions.php
@@ -1,5 +1,17 @@
 <h1>Value of Education</h1>
-<h4>Education as related to socioeconomic values.</h4>
-<p>
-	Use the arrow keys to rotate this cube.
-</p>
+<br />
+<h3>An interactive analysis of factors that contribute to academic success</h3>
+<br />
+<p>Use the left and right arrow keys <br />to see all the sides of this issue.</p>
+
+<br />
+<br />
+<br />
+<br />
+
+<p>Contributors: </p>
+<ul class="byline">
+<li><a href="">Urvashi Goverdhan</a></li> 
+<li><a href="">Perron Jones</a></li> 
+<li><a href="http://andyhub.com/">Andrew Nelson</a></li>
+</ul>
